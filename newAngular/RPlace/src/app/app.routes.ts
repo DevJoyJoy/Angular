78f325457c9a9c832badf3398c144822a17ca84e
“DevJoyJoy” <joycenascimento2007@gmail.com>
2026-04-02 15:45:18 -0300

    Start rplace

--- newAngular/RPlace/src/app/app.routes.ts
+++ newAngular/RPlace/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './features/login-page/login-page';
+import { MainPage } from './features/main-page/main-page';
+import { NotFoundPage } from './features/not-found-page/not-found-page';
+import { RegisterPage } from './features/register-page/register-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: MainPage},
+    {path: "Login", component: LoginPage},
+    {path: "Register", component: RegisterPage},
+    {path: "**", component: NotFoundPage}
+
+];
--- newAngular/RPlace/src/app/features/main-page/main-page.ts
+++ newAngular/RPlace/src/app/features/main-page/main-page.ts
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './main-page.html',
   styleUrl: './main-page.css',
 })
-export class MainPage {}
+export class MainPage {
+  items = [1, 2, 3]
+}
